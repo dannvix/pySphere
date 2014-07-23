92a15661868fee99da3c423afa0ef0063d679787
Shao-Chung Chen <dannvix@gmail.com>
2014-07-23 17:27:27 +0800

    update: package metadata in setup.py

--- setup.py
+++ setup.py
@@ -6,12 +6,9 @@ try:
 except:
     from distutils.core import setup
 
-
 VERSION = (0, 1, 8)
 
-
 if __name__ == "__main__":
-
     try:
         if sys.argv[1] != "install":
             fd = open("pysphere/version.py", "w")
@@ -22,7 +19,7 @@ if __name__ == "__main__":
     except:
         pass
 
-    fd = open("README", "r")
+    fd = open("README.md", "r")
     long_desc = fd.read()
     fd.close()
 
@@ -30,16 +27,13 @@ if __name__ == "__main__":
         name="pysphere",
         version=".".join(["%d" % v for v in VERSION]),
         license="New BSD License",
-        packages=["pysphere", "pysphere.resources", "pysphere.ZSI",
-                  "pysphere.ZSI.wstools", "pysphere.ZSI.generate"],
+        packages=["pysphere", "pysphere.resources", "pysphere.ZSI", "pysphere.ZSI.wstools", "pysphere.ZSI.generate"],
         package_data={'pysphere.ZSI': ['LBNLCopyright']},
-        description="Python API for interacting with the vSphere Web Services SDK",
-        author="Sebastian Tello",
-        author_email="argos83@gmail.com",
-        url="http://pysphere.googlecode.com",
-        download_url="http://pysphere.googlecode.com/files/pysphere-0.1.8.zip",
-        keywords = ["vSphere", "Virtual", "vmware", "ESX", "ESXi",
-                    "VirtualCenter", "SDK", "API"],
+        description="VMware vSphere Web Service client in Python",
+        author="Sebastian Tello <argos83@gmail.com>, Shao-Chung Chen <dannvix@gmail.com>",
+        url="https://github.com/dannvix/pySphere",
+        download_url="https://github.com/dannvix/pySphere/archive/v0.1.8.zip",
+        keywords = ["vSphere", "Virtual", "vmware", "ESX", "ESXi", "VirtualCenter", "SDK", "API"],
         classifiers = [
             "Programming Language :: Python",
             "Programming Language :: Python :: 2.5",
